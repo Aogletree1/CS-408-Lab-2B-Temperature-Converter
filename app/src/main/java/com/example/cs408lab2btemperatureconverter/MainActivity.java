bbd35cbffdc999b4ca930f3a5e699a421a03f838
Aogletree1 <ogletree.austin@yahoo.com>
2023-02-09 01:19:16 -0600

    Mostly Working.

--- app/src/main/java/com/example/cs408lab2btemperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/cs408lab2btemperatureconverter/MainActivity.java
@@ -15,7 +15,13 @@ import com.example.cs408lab2btemperatureconverter.databinding.ActivityMainBindin
 public class MainActivity extends AppCompatActivity {
 
 
+private float celsius;
+private float fahrenheit;
 
+EditText valueReportFahrenheit;
+EditText valueReportCelsius;
+
+    DecimalFormat df = new DecimalFormat(".0");
 
 
 
@@ -35,6 +41,47 @@ public class MainActivity extends AppCompatActivity {
         String tag = view.getTag().toString();
 
             if (tag.equals("buttonConvert")) {
+
+                valueReportFahrenheit = findViewById(R.id.reportFahrenheit);
+                valueReportCelsius = findViewById(R.id.reportCelsius);
+
+                String strFahrenheit = valueReportFahrenheit.getText().toString();
+                String strCelsius = valueReportCelsius.getText().toString();
+
+
+
+
+                if(valueReportFahrenheit == null && valueReportCelsius == null){
+
+
+                }
+
+
+                if(valueReportFahrenheit != null){
+
+
+                    fahrenheit = Float.parseFloat(strFahrenheit);
+
+                    celsius = ((fahrenheit - 32) * 5/9);
+
+
+                    binding.reportCelsius.setText(String.valueOf(df.format(celsius)));
+                    binding.reportFahrenheit.setText(String.valueOf(df.format(fahrenheit)));
+
+
+
+                }
+
+                if(valueReportFahrenheit == null) {
+
+
+                    //fahrenheit = (celsius * 9/5) + 32;
+
+
+                }
+
+
+
 
 
         }
